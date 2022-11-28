af90d8797643a2a910dc67ee7099bd6468078ada
siderasA <andreasideras@gmail.com>
2022-11-28 14:13:56 +0200

    project initialization

--- data_loader_ML.py
+++ data_loader_ML.py
@@ -6,12 +6,13 @@ warnings.filterwarnings('ignore')
 
 class DataLoaderML:
     def __init__(self):
-        self.heart_audio_path = r"C:\Users\ANDRE\OneDrive\Desktop\Andreas_Sideras\Demokritos\Msc in AI\1st Semester\Machine Learning\Assignment\Heart Mumur Classification\the-circor-digiscope-phonocardiogram-dataset-1.0.3\training_data"
-        self.annotations_path = r"C:\Users\ANDRE\OneDrive\Desktop\Andreas_Sideras\Demokritos\Msc in AI\1st Semester\Machine Learning\Assignment\Heart Mumur Classification\the-circor-digiscope-phonocardiogram-dataset-1.0.3"
+        self.heart_audio_path =os.path.join(os.getcwd(), r"the-circor-digiscope-phonocardiogram-dataset-1.0.3\training_data")
+        self.annotations_path =os.path.join(os.getcwd(), r"the-circor-digiscope-phonocardiogram-dataset-1.0.3")
         self.annotations = pd.read_csv(os.path.join(self.annotations_path, "training_data.csv"))[['Patient ID', 'Murmur']]
         self.audios = pd.DataFrame(columns=['Patient_ID', 'AV', 'MV', 'PV', 'TV', 'MURMUR'])
 
     def __create_dataset(self):
+        print(f"Please wait. Out of {self.annotations.shape[0]} patient audios,")
         for index, row in self.annotations.iterrows():
             patient_id = row['Patient ID']
             murmur = row['Murmur']
@@ -48,11 +49,8 @@ class DataLoaderML:
 
             self.audios = self.audios.append(new_row, ignore_index=True)
 
+            if index % 100 == 0:
+                print(f"{index} patient audios have been loaded")
 
     def get_audios(self):
         self.__create_dataset()
-
-
-test = DataLoaderML()
-
-test.get_audios()
